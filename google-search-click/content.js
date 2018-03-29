835180913c602fb5df5c819980b0c9358859a820
Christopher Brown <io@henrian.com>
2018-03-29 13:21:05 -0500

    Use google search click interception to clean up URLs and remove [ping] attributes

--- google-search-click/content.js
+++ google-search-click/content.js
@@ -1,13 +1,41 @@
+/** See documentation in `../README.md` (https://git.io/vxwiJ) */
+
+const trackingTokenRegExp = /[\?\&]utm_(source|medium|term|campaign|content|cid|reader)=[^&#]+/ig
+
+function cleanUrl(url) {
+  let cleanedUrl = url.replace(trackingTokenRegExp, '')
+  // if we deleted the '?', leaving an '&', replace that with a '?'
+  const querystringIndex = url.indexOf('?')
+  if (cleanedUrl[querystringIndex] == '&') {
+    cleanedUrl = cleanedUrl.slice(0, querystringIndex) + '?' + cleanedUrl.slice(querystringIndex + 1)
+  }
+  return cleanedUrl
+}
+
 function initializeListeners() {
-  let currentHref = null
+  let currentUrl = null
   const storeTargetHref = ev => {
-    currentHref = ev.target.href
+    const anchor = ev.target.closest('a')
+    if (anchor) {
+      anchor.removeAttribute('ping')
+      if (anchor.href) {
+        currentUrl = cleanUrl(anchor.href)
+      }
+    }
   }
   const injectStoredHref = ev => {
-    ev.target.href = currentHref
+    const anchor = ev.target.closest('a')
+    if (anchor) {
+      if (currentUrl) {
+        anchor.href = currentUrl
+      }
+    }
   }
-  addEventListener('mousedown', storeTargetHref, true)
-  addEventListener('mousedown', injectStoredHref, false)
+  // because we're implicitly adding the event listener to the `window`,
+  // the events' `currentTarget` will always be `window` (not useful)
+  addEventListener('mousedown', storeTargetHref, true) // useCapture=true
+  // Google does its click-interception somewhere between these
+  addEventListener('mousedown', injectStoredHref, false) // useCapture=false (the default)
 }
 
 document.addEventListener('DOMContentLoaded', () => {
